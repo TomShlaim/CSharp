da150f4d46cbaac4409b1bc796f684106635009f
TomShlaim <tom.shlaim@is.com>
2023-04-15 11:26:59 +0300

    Ex01_02 & Ex01_03 Solution

---
+++ 1/B23 Ex01 AmirKaplan 208789172 TomShlaim 318734845/Ex01_02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Ex01_02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            printDiamond(9);
+        }
+        public static void printDiamond(int i_DiamondHeight)
+        {
+            int initialNumberOfSpaces = 0;
+
+            printLowerDiamond(i_DiamondHeight, initialNumberOfSpaces);
+            if (i_DiamondHeight > 1)
+            {
+                printUpperDiamond(i_DiamondHeight - 2, initialNumberOfSpaces + 1);
+            }
+        }
+
+        private static void printLowerDiamond(int i_DiamondHeight, int i_NumberOfSpaces)
+        {
+            StringBuilder diamondRow = getDiamondRow(i_DiamondHeight, i_NumberOfSpaces);
+
+            if (i_DiamondHeight == 1)
+            {
+                System.Console.WriteLine(diamondRow);
+                return;
+            }
+
+            printLowerDiamond(i_DiamondHeight - 2, i_NumberOfSpaces + 1);
+            System.Console.WriteLine(diamondRow);
+        }
+        private static void printUpperDiamond(int i_DiamondHeight, int i_NumberOfSpaces)
+        {
+            StringBuilder diamondRow = getDiamondRow(i_DiamondHeight, i_NumberOfSpaces);
+
+            System.Console.WriteLine(diamondRow);
+            if (i_DiamondHeight == 1)
+            {
+                return;
+            }
+
+            printUpperDiamond(i_DiamondHeight - 2, i_NumberOfSpaces + 1);
+        }
+        private static StringBuilder getDiamondRow(int i_StarsCount, int i_NumberOfSpaces)
+        {
+            StringBuilder diamondRow = new StringBuilder();
+            diamondRow.Append(new string(' ', i_NumberOfSpaces));
+            diamondRow.Append(new string('*', i_StarsCount));
+            diamondRow.Append(new string(' ', i_NumberOfSpaces));
+
+            return diamondRow;
+        }
+    }
+}
---
+++ 1/B23 Ex01 AmirKaplan 208789172 TomShlaim 318734845/Ex01_03/Program.cs
@@ -0,0 +1,45 @@
+﻿using Ex01_02;
+
+namespace Ex01_03
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            printAdvancedDiamond();
+        }
+        private static void printAdvancedDiamond()
+        {
+            bool isGoodInput = false;
+            int diamondHeightNumber = 0;
+
+            while (!isGoodInput)
+            {
+                System.Console.WriteLine("Please enter the diamond height you would like to build :");
+                string diamondHeightString = System.Console.ReadLine();
+
+                isGoodInput = int.TryParse(diamondHeightString, out diamondHeightNumber);
+
+                if (!isGoodInput)
+                {
+                    System.Console.WriteLine("Invalid number! Please try again.");
+                }
+                if(diamondHeightNumber < 0)
+                {
+                    System.Console.WriteLine("The diamond height can't be negative! Please try again.");
+                    isGoodInput = false;
+                }
+            }
+
+            if(diamondHeightNumber % 2 == 0)
+            {
+                diamondHeightNumber += 1;
+            }
+
+            System.Console.WriteLine("Here is your diamond : ");
+            Ex01_02.Program.printDiamond(diamondHeightNumber);
+        }
+    }
+}
+
+
