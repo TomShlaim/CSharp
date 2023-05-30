13a37a8d5f828d872b92476c490c2c3035f9a158
nic0le-f <nikolfishbein@gmail.com>
2023-05-30 08:34:42 +0300

    EX03 Boilerplate

---
+++ 3/B23 Ex03 AmirKaplan 208789172 TomShlaim 318734845/B23 Ex03 Amir 208789172 Tom 318734845/ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
