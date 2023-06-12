efb1b38bf1e11c817a30729d74044bca032a963e
nic0le-f <nikolfishbein@gmail.com>
2023-06-12 22:31:40 +0300

    Init

---
+++ 4/B23 Ex04 Amir 208789172 Tom 318734845/Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    internal class MainMenu
+    {
+        private string m_Header;
+        private readonly List<MenuItem> m_menuItems;
+
+        public void Show()
+        {
+
+        }
+    }
+}
---
+++ 4/B23 Ex04 Amir 208789172 Tom 318734845/Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    internal class MenuItem
+    {
+        private readonly int r_Id;
+        private readonly List<MenuItem> m_MenuItems = new List<MenuItem>();
+        private readonly List<ILeafMenuItemClickListener> m_LeafMenuItemClickListeners = new List<ILeafMenuItemClickListener>();
+
+        public MenuItem(int i_Id)
+        {
+            r_Id = i_Id;
+        }
+        public MenuItem(int i_Id, List<MenuItem> i_MenuItems)
+        {
+            m_MenuItems.AddRange(i_MenuItems);
+        }
+       private bool IsLeafMenuItem()
+        {
+            return m_MenuItems.Count == 0;
+        }
+        public void Show()
+        {
+
+        }
+        public void AddListener(ILeafMenuItemClickListener i_LeafMenuItemClickListener)
+        {
+            m_LeafMenuItemClickListeners.Add(i_LeafMenuItemClickListener);
+        }
+        public void RemoveListener(ILeafMenuItemClickListener i_LeafMenuItemClickListener)
+        {
+            m_LeafMenuItemClickListeners.Remove(i_LeafMenuItemClickListener);
+        }
+        public void doWhenWasClicked()
+        {
+            if (this.IsLeafMenuItem())
+            {
+                notifyLeafMenuItemClickListeners();    
+            }
+            else
+            {
+                Show();
+            }
+        }
+        private void notifyLeafMenuItemClickListeners()
+        {
+            foreach(ILeafMenuItemClickListener leafMenuItemClickListener in m_LeafMenuItemClickListeners)
+            {
+                leafMenuItemClickListener.ReportLeafMenuItemWasClick(r_Id);
+            }
+        }
+    }
+}
