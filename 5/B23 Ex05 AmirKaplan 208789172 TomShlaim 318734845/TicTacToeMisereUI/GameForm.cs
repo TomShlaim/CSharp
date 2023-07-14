248d914cc7f5e432bc88a41a329ba46a545e05b3
nic0le-f <nikolfishbein@gmail.com>
2023-07-14 17:37:15 +0300

    Game form - Coding standard fixes

--- 5/B23 Ex05 AmirKaplan 208789172 TomShlaim 318734845/TicTacToeMisereUI/GameForm.cs
+++ 5/B23 Ex05 AmirKaplan 208789172 TomShlaim 318734845/TicTacToeMisereUI/GameForm.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Windows.Forms;
 using TicTacToeMisereLogic;
 
@@ -11,31 +9,27 @@ namespace TicTacToeMisereUI
     internal class GameForm : Form
     {
  
-        public readonly TicTacToeMisereLogic.Game r_TicTacToeMisereGame;
+        public readonly Game r_TicTacToeMisereGame;
         private int m_BoardSize;
         private CellButton[,] m_CellButtons;
         private PlayerLabel[] m_LabelPlayers;
 
         public GameForm(int i_BoardSize, List<string> i_PlayerNames)
         {
-            bool isComputerStarting;
-
             m_BoardSize = i_BoardSize;
-            r_TicTacToeMisereGame = new TicTacToeMisereLogic.Game(i_BoardSize, i_PlayerNames);
+            r_TicTacToeMisereGame = new Game(i_BoardSize, i_PlayerNames);
             initializeComponents();
+            bool isComputerStarting = r_TicTacToeMisereGame.Players[r_TicTacToeMisereGame.CurrentTurnPlayerIndex].IsComputer;
 
-            isComputerStarting = r_TicTacToeMisereGame.Players[r_TicTacToeMisereGame.CurrentTurnPlayerIndex].IsComputer;
             if(isComputerStarting) 
             {
                 r_TicTacToeMisereGame.SetComputerPlayerNextChoice();
             }
         }
-
-
-
         private void initializeComponents()
         {
             int formWidth, formHeight, startingPlayerIdx = r_TicTacToeMisereGame.CurrentTurnPlayerIndex;
+
             m_CellButtons = new CellButton[m_BoardSize, m_BoardSize];
             m_LabelPlayers = new PlayerLabel[2];
      
@@ -44,16 +38,16 @@ namespace TicTacToeMisereUI
                 for (int j = 0; j < m_BoardSize; j++)
                 {
                     m_CellButtons[i, j] = new CellButton(i, j, 52 * j + 7, 52 * i + 7);
-                    this.Controls.Add(m_CellButtons[i, j]);
-                    m_CellButtons[i, j].Click += this.cellButton_Click;
+                    Controls.Add(m_CellButtons[i, j]);
+                    m_CellButtons[i, j].Click += cellButton_Click;
                     r_TicTacToeMisereGame.CellOccupied += m_CellButtons[i, j].game_CellOccupied;
                 }
             }
 
             formWidth = (m_CellButtons[0, m_BoardSize - 1].Location.X + m_CellButtons[0, m_BoardSize - 1].Width + 21) - (m_CellButtons[0, 0].Location.X - 7);
             formHeight = (m_CellButtons[m_BoardSize - 1, 0].Location.Y + m_CellButtons[m_BoardSize - 1, 0].Height + 80) - (m_CellButtons[0, 0].Location.Y - 7);
-            this.Size = new System.Drawing.Size(formWidth, formHeight);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            Size = new System.Drawing.Size(formWidth, formHeight);
+            Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
             /* TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
              tableLayoutPanel.Dock = DockStyle.Bottom;
@@ -63,9 +57,8 @@ namespace TicTacToeMisereUI
             */
 
             // LabelPlayer1:
-
             m_LabelPlayers[0] = new PlayerLabel();
-            m_LabelPlayers[0].Location = new System.Drawing.Point(this.Location.X + this.Width / 5, this.Location.Y + this.ClientSize.Height - 30);
+            m_LabelPlayers[0].Location = new System.Drawing.Point(Location.X + Width / 5, Location.Y + ClientSize.Height - 30);
             //m_LabelPlayer1.Anchor = AnchorStyles.Bottom;
             // m_LabelPlayer1.Size = new System.Drawing.Size(91, 31);
             //m_LabelPlayer1.TabIndex = 10;
@@ -73,9 +66,8 @@ namespace TicTacToeMisereUI
             //m_LabelPlayer1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
 
             // LabelPlayer2:
-
             m_LabelPlayers[1] = new PlayerLabel();
-            m_LabelPlayers[1].Location = new System.Drawing.Point(this.Location.X + this.Width * 3 / 5, m_LabelPlayers[0].Location.Y);
+            m_LabelPlayers[1].Location = new System.Drawing.Point(Location.X + Width * 3 / 5, m_LabelPlayers[0].Location.Y);
             // m_LabelPlayer2.Anchor = AnchorStyles.Bottom;
             //  m_LabelPlayer2.Size = new System.Drawing.Size(91, 31);
             //m_LabelPlayer1.TabIndex = 10;
@@ -84,82 +76,67 @@ namespace TicTacToeMisereUI
 
             for(int i = 0; i < m_LabelPlayers.Length; i++)
             {
-
-                System.Drawing.FontStyle fontStyle = startingPlayerIdx == i ? System.Drawing.FontStyle.Bold : System.Drawing.FontStyle.Regular;
+                System.Drawing.FontStyle fontStyle = startingPlayerIdx == i ? 
+                    System.Drawing.FontStyle.Bold : 
+                    System.Drawing.FontStyle.Regular;
                 m_LabelPlayers[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, fontStyle, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                 r_TicTacToeMisereGame.MoveCompleted += m_LabelPlayers[i].game_MoveCompleted;
-                this.Controls.Add(m_LabelPlayers[i]);
+                Controls.Add(m_LabelPlayers[i]);
             }
 
-
-            //tableLayoutPanel.Controls.Add(m_LabelPlayer1, 0, 0);
-            // tableLayoutPanel.Controls.Add(m_LabelPlayer2, 1, 0);
-            // this.Controls.Add(tableLayoutPanel);
-
-
-
-            this.Text = "TicTacToeMisere";
-            this.StartPosition = FormStartPosition.CenterScreen;
-            r_TicTacToeMisereGame.GameDecided += this.game_GameDecided;
-            r_TicTacToeMisereGame.GameTied += this.game_GameTied;
+            Text = "TicTacToeMisere";
+            StartPosition = FormStartPosition.CenterScreen;
+            r_TicTacToeMisereGame.GameDecided += game_GameDecided;
+            r_TicTacToeMisereGame.GameTied += game_GameTied;
         }
 
 
         private void game_GameDecided(object sender, int i_WinnerPlayerIdx)
         {
             MessageBoxButtons messageBoxButtons = MessageBoxButtons.YesNo;
-            DialogResult result;
 
-            this.updatePlayerLabelText(i_WinnerPlayerIdx);
-            result = MessageBox.Show(string.Format(
+            updatePlayerLabelText(i_WinnerPlayerIdx);
+
+            DialogResult result = MessageBox.Show(string.Format(
 @"The winner is {0}!
 Would you like to play another round?", r_TicTacToeMisereGame.Players[i_WinnerPlayerIdx].Name), "A Win!", messageBoxButtons);
+
             if (result == DialogResult.No)
             {
-                this.Close();
+                Close();
             }
             else if (result == DialogResult.Yes)
             {
                 r_TicTacToeMisereGame.Rematch();
-                this.clearBoard();    
+                clearBoard();    
             }
-
         }
 
         private void game_GameTied(object sender, EventArgs e)
         {
             MessageBoxButtons messageBoxButtons = MessageBoxButtons.YesNo;
-            DialogResult result;
-
-            result = MessageBox.Show(string.Format(
+            DialogResult result = MessageBox.Show(string.Format(
 @"A Tie!
 Would you like to play another round?"), "A Tie!", messageBoxButtons);
+
             if (result == DialogResult.No)
             {
-                this.Close();
+                Close();
             }
             else if (result == DialogResult.Yes)
             {
                 r_TicTacToeMisereGame.Rematch();
-                this.clearBoard();
+                clearBoard();
             }
         }
-
-       /*
-        private void game_Check(object sender, EventArgs e)
-        {
-            DialogResult result = MessageBox.Show("CHECK");
-        }
-       */
-
         private void cellButton_Click(object sender, EventArgs e)
         {
             CellButton cellButton = sender as CellButton;
             Cell chosenCell = new Cell(cellButton.Row, cellButton.Column);
-            bool isCurrentPlayerComputer;
 
             r_TicTacToeMisereGame.SetPersonPlayerNextChoice(chosenCell);
-            isCurrentPlayerComputer = r_TicTacToeMisereGame.Players[r_TicTacToeMisereGame.CurrentTurnPlayerIndex].IsComputer;
+            bool isCurrentPlayerComputer = r_TicTacToeMisereGame.Players[r_TicTacToeMisereGame.CurrentTurnPlayerIndex].IsComputer;
+
             if (isCurrentPlayerComputer)
             {
                 r_TicTacToeMisereGame.SetComputerPlayerNextChoice();
@@ -176,18 +153,6 @@ Would you like to play another round?"), "A Tie!", messageBoxButtons);
                 cellButton.Enabled = true;
             }
         }
-        
-        /*
-        private void updatePlayerLabelsFont()
-        {
-            int currentPlayerTurnIdx, prevPlayerTurnIdx;
-
-            currentPlayerTurnIdx = r_TicTacToeMisereGame.CurrentTurnPlayerIndex;
-            prevPlayerTurnIdx = currentPlayerTurnIdx == 0 ? m_LabelPlayers.Length - 1 : currentPlayerTurnIdx - 1;
-            m_LabelPlayers[currentPlayerTurnIdx].Font = new System.Drawing.Font(m_LabelPlayers[currentPlayerTurnIdx].Font, System.Drawing.FontStyle.Bold);
-            m_LabelPlayers[prevPlayerTurnIdx].Font = new System.Drawing.Font(m_LabelPlayers[prevPlayerTurnIdx].Font, System.Drawing.FontStyle.Regular);
-        }*/
-
         private void updatePlayerLabelText(int i_LabelIdx)
         {
             m_LabelPlayers[i_LabelIdx].Text = string.Format(
